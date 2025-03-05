59595e086c602d97af0179fb6b943e56bd260617
= <=>
2025-03-05 17:06:58 +0100

    Program z lab0

--- PP_NET/Program.cs
+++ PP_NET/Program.cs
@@ -1,13 +1,10 @@
-﻿namespace PP_NET
-{
-    class FizzBuzz
-    {
+﻿using System.Security.Cryptography;
+
+namespace PP_NET{
+    class FizzBuzz{
         private int _number;
         public FizzBuzz() => this._number = 15;
-        public FizzBuzz(int number)
-        {
-            _number = number;
-        }
+        public FizzBuzz(int number) => this._number = number;
         public void GetFizzBuzz(){
             for (int i = 1; i <= _number; i++){
                 if (i % 3 == 0 && i % 5 == 0) {
@@ -22,10 +19,9 @@
             }
         }
     }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+    internal class Program{
+        static void Main(string[] args){
+            int number
             FizzBuzz fizzBuzz = new FizzBuzz();
             fizzBuzz.GetFizzBuzz();
         }
