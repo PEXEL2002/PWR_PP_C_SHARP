4539c13ec544726e7ac13af1a26ff20dadc452d2
= <=>
2025-03-05 08:44:08 +0100

    Dodaj pliki projektów.

---
+++ PP_NET/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PP_NET
+{
+    class FizzBuzz
+    {
+        private int _number;
+        public FizzBuzz() => this._number = 15;
+        public FizzBuzz(int number)
+        {
+            _number = number;
+        }
+        public void GetFizzBuzz(){
+            for (int i = 1; i <= _number; i++){
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                } else if (i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                } else if (i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.GetFizzBuzz();
+        }
+    }
+}
